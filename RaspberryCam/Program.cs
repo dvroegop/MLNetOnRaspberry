a1ddaf94ad633410749b35f01f58988c48e2138e
Dennis Vroegop <dennis@vroegop.org>
2021-04-19 16:33:03 +0200

    Add project files.

---
+++ RaspberryCam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaspberryCam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
